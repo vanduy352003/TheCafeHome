8408156d2edc2668c59ba8dfee25fbd78840f1b3
Van Duy <vanduy352003@gmail.com>
2024-05-12 21:22:53 +0700

    tải package để làm bottomsheet

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App';
 import {name as appName} from './app.json';
 import ShopInformation from './src/screens/ShopInformation';
 
-AppRegistry.registerComponent(appName, () => ShopInformation);
+AppRegistry.registerComponent(appName, () => App);
